799a6e9b878fe1e123705cd0d78a0893f699cdfd
dannycahyo <dannydwicahyono@gmail.com>
2023-03-11 08:06:21 +0700

    feat: add another solution for minimum absolute difference question

--- leet-code/minimumAbsoluteDifference.js
+++ leet-code/minimumAbsoluteDifference.js
@@ -57,4 +57,23 @@ function minimumAbsoluteDifference(arr) {
   return result;
 }
 
-console.log(minimumAbsoluteDifference([3, 8, -10, 23, 19, -4, -14, 27]));
+var minimumAbsDifference = function (arr) {
+  let minAblosute = Infinity;
+  let output = [];
+
+  arr = arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    const minus = arr[i + 1] - arr[i];
+    if (minus < minAblosute) {
+      minAblosute = minus;
+      output = [[arr[i], arr[i + 1]]];
+    } else if (minus === minAblosute) {
+      output.push([arr[i], arr[i + 1]]);
+    }
+  }
+
+  return output;
+};
+
+console.log(minimumAbsDifference([3, 8, -10, 23, 19, -4, -14, 27]));
