56409a746bed826cfc2546d9975ee30897ae9506
dannycahyo <dannydwicahyono@gmail.com>
2023-07-05 16:44:17 +0700

    fix: adjust fibbonaci code & add new solution

--- dynamic-programming/fibonacci.ts
+++ dynamic-programming/fibonacci.ts
@@ -1,7 +1,11 @@
 // Plain Recursion
 function fib(n: number) {
-  if (n < 2) {
-    return n;
+  if (n === 2) {
+    return 1;
+  }
+
+  if (n === 1) {
+    return 0;
   }
 
   return fib(n - 1) + fib(n - 2);
@@ -10,36 +14,59 @@ function fib(n: number) {
 console.log(fib(5));
 
 // Memoized Solution
-function memoized_fib(n: number, memo: number[]) {
+function memoizedFib(n: number, memo: number[] = []) {
   if (memo[n] !== undefined) {
     return memo[n];
   }
 
-  let result;
+  let result: number;
 
-  if (n < 2) {
-    result = n;
+  if (n === 2) {
+    return 1;
+  } else if (n === 1) {
+    return 0;
   } else {
-    result = memoized_fib(n - 1, memo) + memoized_fib(n - 2, memo);
+    result = memoizedFib(n - 1, memo) + memoizedFib(n - 2, memo);
   }
 
   memo[n] = result;
   return result;
 }
 
+console.log(memoizedFib(6));
+
 function fibBottomUp(n: number) {
   if (n < 2) {
     return n;
   }
-  let bottomUp: number[] = [];
-  bottomUp[1] = 1;
-  bottomUp[2] = 1;
 
-  for (let i = 3; i < n + 1; i++) {
-    bottomUp[i] = bottomUp[i - 1] + bottomUp[i - 2];
+  let dp: number[] = [];
+  dp[0] = 0;
+  dp[1] = 1;
+
+  for (let i = 2; i < n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
   }
 
-  return bottomUp[n];
+  return dp[n - 1];
 }
+
+// Iterative but better for space complexity
+function getFibN(n: number): number {
+  const lastTwoFibNum = [0, 1];
+
+  let counter = 3;
+
+  while (counter <= n) {
+    const nextFib = lastTwoFibNum[0] + lastTwoFibNum[1];
+    lastTwoFibNum[0] = lastTwoFibNum[1];
+    lastTwoFibNum[1] = nextFib;
+    counter++;
+  }
+
+  return n > 1 ? lastTwoFibNum[1] : lastTwoFibNum[0];
+}
+
+console.log(getFibN(5));
 
 console.log(fibBottomUp(5));
